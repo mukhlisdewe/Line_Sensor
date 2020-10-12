03e0b8b1f22297ddc96faffd27d74927f09f79a6
mukhlisdewe <45230164+mukhlisdewe@users.noreply.github.com>
2020-10-13 05:36:06 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@
 
 double PV=0;
 char* t[6];
+int data_sensor[11][10];
+int data_avg[11];
 
 
 static void gpio_init(void){
@@ -48,31 +50,72 @@ static void uart_transmits(char* s){
     s++;
   }
 }
+static void adc_init(uint8_t p){
+  ADCSRA |= (1<<ADEN);
+  ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2);
 
-uint16_t baca_sensor (){
-  uint16_t sensor=0;
-  
+  ADMUX = 0x80;
+  ADMUX = 0x80 | p;
+}
+
+static uint16_t adc_read(uint8_t p){
+  uint16_t adc_avg = 0;
+  adc_init(p);
+  ADCSRA |= (1<<ADSC);
+  for(uint8_t x=0;x<10;x++){
+    while(ADCSRA & (1<<ADSC));
+    adc_avg += ADCW;
+  }
+  return (adc_avg/10);
+}
+
+uint8_t baca_sensor(){
+  int temp = 0;
   PORTD |= SW_PIN_LEFT;
-  if((PINC & (1<<0))==0)sensor=sensor |0b10000000000;
-  if((PINC & (1<<1))==0)sensor=sensor |0b01000000000;
-  if((PINC & (1<<2))==0)sensor=sensor |0b00100000000;
-  if((PINC & (1<<3))==0)sensor=sensor |0b00010000000;
-  if((PINC & (1<<4))==0)sensor=sensor |0b00001000000;
-  if((PINC & (1<<5))==0)sensor=sensor |0b00000100000;
+    for(int y=0;y<5;y++){
+      for(int x=0;x<10;x++){
+        data_sensor[y][x] = adc_read(y);
+      }
+    }
   PORTD &= ~SW_PIN_LEFT;
   PORTD |= SW_PIN_RIGHT;
-  if((PINC & (1<<0))==0)sensor=sensor |0b00000000001;
-  if((PINC & (1<<1))==0)sensor=sensor |0b00000000010;
-  if((PINC & (1<<2))==0)sensor=sensor |0b00000000100;
-  if((PINC & (1<<3))==0)sensor=sensor |0b00000001000;
-  if((PINC & (1<<4))==0)sensor=sensor |0b00000010000;
+    for(int y=0;y<6;y++){
+      for(int x=0;x<10;x++){
+        data_sensor[y][x] = adc_read(y);
+      }
+    }
   PORTD &= ~SW_PIN_RIGHT;
+
+  for(int y=0;y<11;y++){
+  for(int x=0;x<10;x++){
+    temp += data_sensor[y][x];
+  }
+  
+  temp /= 10;
+  data_avg[y] = temp;
+  temp = 0;
+}
+}
+
+uint16_t map_sensor (){
+  uint16_t sensor=0;
+  if(baca_sensor()==0)sensor=sensor |0b10000000000;
+  if(baca_sensor()==0)sensor=sensor |0b01000000000;
+  if(baca_sensor()==0)sensor=sensor |0b00100000000;
+  if(baca_sensor()==0)sensor=sensor |0b00010000000;
+  if(baca_sensor()==0)sensor=sensor |0b00001000000;
+  if(baca_sensor()==0)sensor=sensor |0b00000100000;
+  if(baca_sensor()==0)sensor=sensor |0b00000000001;
+  if(baca_sensor()==0)sensor=sensor |0b00000000010;
+  if(baca_sensor()==0)sensor=sensor |0b00000000100;
+  if(baca_sensor()==0)sensor=sensor |0b00000001000;
+  if(baca_sensor()==0)sensor=sensor |0b00000010000;
 
   return sensor;
 }
 
 int map_case(){
-  switch(baca_sensor()) {
+  switch(map_sensor()) {
   
     case 0b00000000001  :   PV= 30; break;
     case 0b00000000011  :   PV= 5; break;
@@ -159,21 +202,24 @@ void pid (){
 int main(void){
   gpio_init();
   uart_init();
-  uint8_t x;
-  uart_transmit(((PIND & (1<<6)) + 48));
-  int a;
+  gpio_write((LED_PIN_LEFT | LED_PIN_RIGHT),1);
+  char tampil[10];
+  // uart_transmit(((PIND & (1<<6)) + 48));
+
 
   while(1){
     // break;
-    gpio_write((LED_PIN_LEFT | LED_PIN_RIGHT),1);
-    map_case();
-    pid();
-    for (x=0;x<6;x++){
-      uart_transmits(t[x]);
+    // map_case();
+    // pid();
+    // for (x=0;x<6;x++){
+    //   uart_transmits(t[x]);
+    //   uart_transmit(';');
+    // }
+    for(int x=0;x<11;x++){
+      itoa(data_avg[x], tampil, 10);
+      uart_transmits(tampil);
       uart_transmit(';');
     }
-    // a = PINC & (1<<0);
-    // uart_transmits(sensor);
     uart_transmits((char*)"\r\n");// carriege return // newline
   }
 }
